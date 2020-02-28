cabc94cc4097b7654e4512004e0d4a5c11f8cc91
IharLobach <53921633+IharLobach@users.noreply.github.com>
2020-02-28 14:07:48 -0600

    added caching time delay files

--- ursse/data_processing.py
+++ ursse/data_processing.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import os
 from ursse.hydra_harp_file_reader import HydraHarpFile
 from ursse.LED_tests.data_analyzis import \
  calc_Fano, get_time_window_hist, calc_Fano_from_counts_per_time_window
@@ -32,6 +33,38 @@ def get_event_delays(f, channel=1):
     return pd.DataFrame(
         {"revolution": revolution_counts_only, "delay": t_delays.values},
         index=t_delays.index), revolution[-1]+1
+
+
+def save_event_delays(file_path, channel=1):
+    f = HydraHarpFile(file_path, safemode=False)
+    df, n_revolutions = get_event_delays(f, channel)
+    df1 = pd.DataFrame({"revolution": [n_revolutions], "delay": [0]})
+    df.append(df1, ignore_index=True)
+    dir_name = os.path.dirname(file_path)
+    cache_folder = os.path.join(dir_name, "cache")
+    if not os.path.exists(cache_folder):
+        os.mkdir(cache_folder)
+    f_name = os.path.basename(file_path)
+    pkl_name = f_name.replace("ptu", "pkl")
+    pkl_path = os.path.join(cache_folder, pkl_name)
+    df.to_pickle(pkl_path)
+    return df, n_revolutions
+
+
+def read_event_delays(file_path):
+    dir_name = os.path.dirname(file_path)
+    cache_folder = os.path.join(dir_name, "cache")
+    f_name = os.path.basename(file_path)
+    pkl_name = f_name.replace("ptu", "pkl")
+    pkl_path = os.path.join(cache_folder, pkl_name)
+    if os.path.exists(pkl_path):
+        df = pd.read_pickle(pkl_path)
+        n_revolutions = df.iloc[-1, 0]
+        df = df.iloc[:-1, :]
+        return df, n_revolutions
+    else:
+        return save_event_delays(file_path)
+
 
 
 def plot_arrival_time_hist(t_delays, gate, bins=None,
--- ursse/path_assistant.py
+++ ursse/path_assistant.py
@@ -11,27 +11,37 @@ def show_shift_folders():
         print(i, f)
 
 
-def get_shift_path(shift_index):
-    return os.path.join(shifts_folder, shift_folders[shift_index],
+def get_shift_path(shift_folder_name):
+    return os.path.join(shifts_folder, shift_folder_name,
                         "time_stamp_files")
 
 
-def get_shift_files(shift_index):
-    shift = get_shift_path(shift_index)
-    files = os.listdir(shift)
+def get_shift_cache_folder_path(shift_folder_name):
+    return os.path.join(get_shift_path(shift_folder_name), "cache")
+    
+
+def get_shift_files(shift_folder_name):
+    shift = get_shift_path(shift_folder_name)
+    files = [f for f in os.listdir(shift) \
+        if os.path.isfile(os.path.join(shift, f))]
     return files
 
 
-def show_time_stamp_file_names(shift_index):
-    files = get_shift_files(shift_index)
+def show_time_stamp_file_names(shift_folder_name):
+    files = get_shift_files(shift_folder_name)
     for i, f in enumerate(files):
         print(i, f)
 
 
-def get_time_stamp_file_paths(shift_index):
-    shift = get_shift_path(shift_index)
-    files = get_shift_files(shift_index)
+def get_time_stamp_file_paths(shift_folder_name):
+    shift = get_shift_path(shift_folder_name)
+    files = get_shift_files(shift_folder_name)
     return [os.path.join(shift, f) for f in files]
+
+
+def get_time_stamp_file_path(shift_folder_name, time_stamp_file_name):
+    shift = get_shift_path(shift_folder_name)
+    return os.path.join(shift, time_stamp_file_name)
 
 
 class WorkingDirectory():
