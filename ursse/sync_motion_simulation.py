ca9a75886ddcd3800953295e511dcef5041a8747
Ihar Lobach <lobachihor1994@gmail.com>
2021-01-01 21:24:22 -0600

    made functions to calculate amplitude and rms histograms

--- ursse/sync_motion_simulation.py
+++ ursse/sync_motion_simulation.py
@@ -6,6 +6,7 @@ import ursse_cpp.sync_motion_sim as sm
 from config_ursse import get_from_config
 from ursse.path_assistant import PathAssistant
 import os
+from scipy.stats import chisquare
 
 
 def calc_sim_df_one_file(shift, file, rf_noise_std, tau0=None, delta0=None,
@@ -39,7 +40,7 @@ def calc_sim_df_one_file(shift, file, rf_noise_std, tau0=None, delta0=None,
 
 
 def add_spad_tts_to_sim_df(sim_df, spad_tts=0.35, mean_spad=0.5,
-                           show_spad_tt_dist=False):
+                           show_spad_tt_dist=False, np_rand_seed=None):
     """Adds random delays to the delays (photon arrival times) in sim_df. The added delays are drawn from a Gamma distribtuion with mean=spad_mean and std=spad_tts
 
     Args:
@@ -51,6 +52,8 @@ def add_spad_tts_to_sim_df(sim_df, spad_tts=0.35, mean_spad=0.5,
     Returns:
         [type]: [description]
     """
+    np.random.seed(np_rand_seed)
+    sim_df = sim_df.copy()
     std_spad = spad_tts
     theta_spad = std_spad**2/mean_spad
     k_spad = mean_spad/theta_spad
@@ -81,44 +84,192 @@ def calc_sim_df_several_files(rf_noise_std, files_and_pars=None, V=None):
                 "file": '1el_002.ptu',
                 "tau0": None,
                 "delta0": None,
-                "rand_seed_int": 1
+                "rand_seed_int": 1,
+                "spad_tts_rand_seed": None
              },
             {
                 "shift": 'shift_02_28_2020',
                 "file": '1el_000.ptu',
                 "tau0": None,
                 "delta0": None,
-                "rand_seed_int": 2
+                "rand_seed_int": 2,
+                "spad_tts_rand_seed": None
              },
             {
                 "shift": 'shift_02_28_2020',
                 "file": '1el_001.ptu',
                 "tau0": None,
                 "delta0": None,
-                "rand_seed_int": 3
+                "rand_seed_int": 3,
+                "spad_tts_rand_seed": None
              },
             {
                 "shift": 'shift_03_05_2020',
                 "file": '1el_filters_000.ptu',
                 "tau0": None,
                 "delta0": None,
-                "rand_seed_int": 4
+                "rand_seed_int": 4,
+                "spad_tts_rand_seed": None
              },
             {
                 "shift": 'shift_03_05_2020',
                 "file": '1el_filters_008.ptu',
                 "tau0": None,
                 "delta0": None,
-                "rand_seed_int": 5
+                "rand_seed_int": 5,
+                "spad_tts_rand_seed": None
              }
         ]
     for i, el in enumerate(files_and_pars):
         print(f"working on {i+1} out of {len(files_and_pars)}")
-        el['meas_df'], el['sim_df'] = calc_sim_df_one_file(el['shift'],
+        el['meas_df'], el['sim_df_before_spad'] = calc_sim_df_one_file(
+                                            el['shift'],
                                             el['file'],
                                             rf_noise_std,
                                             el['tau0'],
                                             el['delta0'],
                                             el['rand_seed_int'],
                                             V)
+        el['sim_df'] = el['sim_df_before_spad'].copy()
     return files_and_pars
+
+
+def add_spad_tts_do_fitting_and_binning(files_and_pars,
+                           spad_tts=0.35,
+                           mean_spad=0.5,
+                           verbose=False,
+                           rms_size_dt_sec=0.1):
+    for el in files_and_pars:
+        el['sim_df'] = add_spad_tts_to_sim_df(
+            el['sim_df_before_spad'], spad_tts, mean_spad,
+            np_rand_seed=el['spad_tts_rand_seed'])
+    for i, el in enumerate(files_and_pars):
+        if verbose:
+            print(f"working on file number {i+1} out of {len(files_and_pars)}")
+        el["meas_T0"] = pst.get_initial_sync_period_estimate(el["meas_df"])
+        el["meas_phase_df"], el["meas_fits_df"] = \
+            pst.get_phase_df_from_revoluton_delay_df(
+                el["meas_df"], el["meas_T0"])
+        el["meas_sz_df"] = pst.get_sz_df(el["meas_df"], spad_tts_ns=0,
+                                         dt=rms_size_dt_sec)
+        el["meas_polar_df"] = pst.get_polar_df(
+            el["meas_phase_df"], el["meas_sz_df"])
+        el["sim_T0"] = pst.get_initial_sync_period_estimate(el["sim_df"])
+        el["sim_phase_df"], el["sim_fits_df"] = \
+            pst.get_phase_df_from_revoluton_delay_df(
+                el["sim_df"], el["sim_T0"])
+        el["sim_sz_df"] = pst.get_sz_df(el["sim_df"], spad_tts_ns=0,
+                                        dt=rms_size_dt_sec)
+        el["sim_polar_df"] = pst.get_polar_df(
+            el["sim_phase_df"], el["sim_sz_df"])
+    return files_and_pars
+
+
+def get_meas_sim_hist(input_list, nbins=20,
+        do_chi2=True, chi2_min_count=50):
+    el = input_list[0]
+    l = min(el['meas'].min(), el['sim'].min())
+    r = max(el['meas'].max(), el['sim'].max())
+    for el in input_list[1:]:
+        lc = min(el['meas'].min(), el['sim'].min())
+        rc = max(el['meas'].max(), el['sim'].max())
+        if lc < l:
+            l = lc
+        if rc > r:
+            r = rc
+    bins = np.linspace(l, r, nbins)
+    bin_centers = (bins[1:]+bins[:-1])/2
+    hist_list = []
+    for el in input_list:
+        hist_list.append(
+            {'meas': np.histogram(el['meas'], bins=bins)[0],
+             'sim': np.histogram(el['sim'], bins=bins)[0]}
+        )
+    hist_meas = np.zeros(bin_centers.shape)
+    hist_sim = np.zeros(bin_centers.shape)
+    for el in hist_list:
+        hist_meas += el['meas']
+        hist_sim += el['sim']
+    res = {}
+    res['individual_hists'] = hist_list
+    res['aggregated_hists'] = {'meas': hist_meas, 'sim': hist_sim}    
+    if do_chi2:
+        above_chi2_min_count = (
+            hist_meas > chi2_min_count) * (hist_sim > chi2_min_count)
+        meas_chi2 = hist_meas[above_chi2_min_count]
+        meas_chi2[-1] += np.sum(hist_meas[~above_chi2_min_count])
+        sim_chi2 = hist_sim[above_chi2_min_count]
+        sim_chi2[-1] += np.sum(hist_sim[~above_chi2_min_count])
+        res['chi-squared'] = chisquare(f_obs=meas_chi2, f_exp=sim_chi2)
+    res['bins'] = bins
+    res['bin_centers'] = bin_centers
+    # calculating mean below
+    means = []
+    lens = []
+    for el in input_list:
+        means.append({
+            'meas': el['meas'].mean(),
+            'sim': el['sim'].mean()
+        })
+        lens.append({
+            'meas': len(el['meas'].index),
+            'sim': len(el['sim'].index)
+        })
+    res['individual_means'] = means
+    meas_tot_len = sum([el['meas'] for el in lens])
+    sim_tot_len = sum([el['sim'] for el in lens])
+    res['tot_means'] = {
+        'meas': sum([a['meas']*b['meas'] for a, b in zip(means, lens)])/meas_tot_len,
+        'sim': sum([a['sim']*b['sim'] for a, b in zip(means, lens)])/sim_tot_len,
+    }
+    
+    return res
+
+
+def get_meas_sim_comparison(files_and_pars, feature, nbins=20,
+                            do_chi2=True, chi2_min_count=50,
+                            show_plot=True):
+    """files_and_pars must have pre-calculated fits and rms length
+
+    Args:
+        files_and_pars ([type]): [description]
+        feature ([type]): [description]
+        nbins (int, optional): [description]. Defaults to 20.
+        do_chi2 (bool, optional): [description]. Defaults to True.
+        chi2_min_count (int, optional): [description]. Defaults to 50.
+
+    Raises:
+        ValueError: [description]
+
+    Returns:
+        [type]: [description]
+    """
+    if feature == "amplitude":
+        input_list = [{'meas': el['meas_fits_df']['Amplitude_ns'], 'sim': el['sim_fits_df']['Amplitude_ns']}
+                      for el in files_and_pars]
+        xlabel = "Amplitude (ns)"
+    elif feature == "rms_length":
+        input_list = [{'meas': el['meas_sz_df']['sz_ns'], 'sim': el['sim_sz_df']['sz_ns']}
+                      for el in files_and_pars]
+        xlabel = "RMS of electron position in a time window"
+    else:
+        raise ValueError("Unknown feature for comparison. Choose from"
+        "amplitude, rms_length")
+    hist_dic = get_meas_sim_hist(input_list, nbins, do_chi2, chi2_min_count)
+    if show_plot:
+        fig, ax = plt.subplots(figsize=(15, 7))
+        bins = hist_dic['bins']
+        bin_centers = hist_dic['bin_centers']
+        ax.hist(bin_centers, weights=hist_dic['aggregated_hists']['meas'], bins=bins, density=False,
+                alpha=0.5, label="Measurement")
+        ax.hist(bin_centers, weights=hist_dic['aggregated_hists']['sim'], bins=bins, density=False,
+                alpha=0.5, label="Simulation")
+        ax.set_xlabel(xlabel)
+        ax.set_ylabel("Occurrences")
+        ax.annotate(f"p-value = {hist_dic['chi-squared'].pvalue}" \
+                    + "\n" + f"meas_mean = {hist_dic['tot_means']['meas']:.2e}"
+                    + "\n" + f"sim_mean = {hist_dic['tot_means']['sim']:.2e}",
+            (0.5,0.5), xycoords='axes fraction')
+        ax.legend()
+        plt.show()
+    return hist_dic
